64d64f285d2893e1d0b5a285ce84d622e2c3b0af
林博仁 <Buo.Ren.Lin@gmail.com>
2017-04-27 16:33:12 +0800

    Source Code/Shell Script(GNU Bash)(source'd).source.bash - 修正 SC2148
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- Source Code/Shell Script(GNU Bash)(source'd).source.bash
+++ Source Code/Shell Script(GNU Bash)(source'd).source.bash
@@ -1,6 +1,7 @@
+#shellcheck shell=bash
+
 # <Source Description>
 # <Copyright Holder Name> © <Copyright Activation Latest Year, for determining year of end of copyright protection>
-#shellcheck shell=bash
 ## Include Guard to prevent multiple sourcing
 ## TODO: rename INCLUDE_GUARD to something sensible for each file, like INCLUDE_GUARD_FOO
 if [ -z "${INCLUDE_GUARD}" ]; then
