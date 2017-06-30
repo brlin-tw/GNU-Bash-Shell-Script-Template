139dcc48ca44d497bc0f69ffc4361f51817b5c87
林博仁 <Buo.Ren.Lin@gmail.com>
2017-06-30 18:39:10 +0800

    Source Code/Shell Script(GNU Bash)(source'd).template.source.bash - Fix include guard checking
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- Source Code/Shell Script(GNU Bash)(source'd).template.source.bash
+++ Source Code/Shell Script(GNU Bash)(source'd).template.source.bash
@@ -5,7 +5,7 @@
 # <Copyright Holder Name> © <Copyright Activation Latest Year, for determining year of end of copyright protection>
 ## Include Guard to prevent multiple sourcing
 ## TODO: rename INCLUDE_GUARD to something sensible for each file, like INCLUDE_GUARD_FOO
-if [ -z "${INCLUDE_GUARD}" ]; then
+if [ -n "${INCLUDE_GUARD}" ]; then
 	return 0
 fi
 
