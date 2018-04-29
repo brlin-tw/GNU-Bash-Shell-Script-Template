6a021dc07c31cbfde3b63efc7f38f1f9a3b44aac
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-04-29 16:32:06 +0800

    Rewrite distribute program
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- distribute.bash
+++ distribute.bash
@@ -1,43 +1,128 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
+
 ## Makes debuggers' life easier - Unofficial Bash Strict Mode
-## http://redsymbol.net/articles/unofficial-bash-strict-mode/
-### Exit immediately if a pipeline, which may consist of a single simple command, a list, or a compound command returns a non-zero status.  The shell does not exit if the command that fails is part of the command list immediately following a `while' or `until' keyword, part of the test in an `if' statement, part of any command executed in a `&&' or `||' list except the command following the final `&&' or `||', any command in a pipeline but the last, or if the command's return status is being inverted with `!'.  If a compound command other than a subshell returns a non-zero status because a command failed while `-e' was being ignored, the shell does not exit.  A trap on `ERR', if set, is executed before the shell exits.
+## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
 set -o errexit
-
-### Treat unset variables and parameters other than the special parameters `@' or `*' as an error when performing parameter expansion.  An error message will be written to the standard error, and a non-interactive shell will exit.
-set -o nounset
-
-### If set, any trap on `ERR' is inherited by shell functions, command substitutions, and commands executed in a subshell environment.  The `ERR' trap is normally not inherited in such cases.
 set -o errtrace
-
-### If set, the return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully.  This option is disabled by default.
+set -o nounset
 set -o pipefail
 
+## Runtime Dependencies Checking
+declare\
+	runtime_dependency_checking_result=still-pass\
+	required_software
+
+for required_command in \
+	basename\
+	dirname\
+	git\
+	mkdir\
+	realpath\
+	tar; do
+	if ! command -v "${required_command}" &>/dev/null; then
+		runtime_dependency_checking_result=fail
+
+		case "${required_command}" in
+			basename\
+			|dirname\
+			|realpath)
+				required_software='GNU Coreutils'
+				;;
+			*)
+				required_software="${required_command}"
+				;;
+		esac
+
+		printf --\
+			'Error: This program requires "%s" to be installed and its executables in the executable searching paths.\n'\
+			"${required_software}" 1>&2
+		unset required_software
+	fi
+done; unset required_command required_software
+
+if [ "${runtime_dependency_checking_result}" = fail ]; then
+	printf --\
+		'Error: Runtime dependency checking fail, the progrom cannot continue.\n' 1>&2
+	exit 1
+fi; unset runtime_dependency_checking_result
+
 ## Non-overridable Primitive Variables
-##
-## BashFAQ/How do I determine the location of my script? I want to read some config files from the same place. - Greg's Wiki
-## http://mywiki.wooledge.org/BashFAQ/028
-RUNTIME_SCRIPT_FILENAME="$(basename "${BASH_SOURCE[0]}")"
-readonly RUNTIME_SCRIPT_FILENAME
-RUNTIME_SCRIPT_DIRECTORY="$(dirname "$(realpath --strip "${0}")")"
-readonly RUNTIME_SCRIPT_DIRECTORY
-readonly RUNTIME_SCRIPT_PATH_ABSOLUTE="${RUNTIME_SCRIPT_DIRECTORY}/${RUNTIME_SCRIPT_FILENAME}"
-readonly RUNTIME_SCRIPT_PATH_RELATIVE="${0}"
-readonly RUNTIME_COMMAND_BASE="${RUNTIME_COMMAND_BASE:-${0}}"
+## BASHDOC: Shell Variables » Bash Variables
+## BASHDOC: Basic Shell Features » Shell Parameters » Special Parameters
+if [ -v 'BASH_SOURCE[0]' ]; then
+	RUNTIME_EXECUTABLE_PATH="$(realpath --strip "${BASH_SOURCE[0]}")"
+	RUNTIME_EXECUTABLE_FILENAME="$(basename "${RUNTIME_EXECUTABLE_PATH}")"
+	RUNTIME_EXECUTABLE_NAME="${RUNTIME_EXECUTABLE_FILENAME%.*}"
+	RUNTIME_EXECUTABLE_DIRECTORY="$(dirname "${RUNTIME_EXECUTABLE_PATH}")"
+	RUNTIME_COMMANDLINE_BASECOMMAND="${0}"
+	declare -r\
+		RUNTIME_EXECUTABLE_FILENAME\
+		RUNTIME_EXECUTABLE_DIRECTORY\
+		RUNTIME_EXECUTABLE_PATHABSOLUTE\
+		RUNTIME_COMMANDLINE_BASECOMMAND
+fi
+declare -ar RUNTIME_COMMANDLINE_PARAMETERS=("${@}")
 
-readonly APPLICATION_IDENTIFIER="gnu-bash-shell-script-template"
-
-for a_command in mkdir tar git; do
-	if ! command -v "${a_command}" &>/dev/null; then
-		printf "%s: Error: %s command not found.\n" "${RUNTIME_SCRIPT_FILENAME}" "${a_command}"
+## init function: entrypoint of main program
+## This function is called near the end of the file,
+## with the script's command-line parameters as arguments
+init(){
+	if ! process_commandline_parameters; then
+		printf --\
+			'Error: %s: Invalid command-line parameters.\n'\
+			"${FUNCNAME[0]}"\
+			1>&2
+		print_help
 		exit 1
 	fi
-done
 
+	declare \
+		GIT_DIR="${RUNTIME_EXECUTABLE_DIRECTORY}/.git" \
+		GIT_WORK_TREE="${RUNTIME_EXECUTABLE_DIRECTORY}"
+	export GIT_DIR GIT_WORK_TREE
+
+	local package_version; package_version="$(determine_package_revision)"
+
+	# shellcheck source=/dev/null
+	source "${RUNTIME_EXECUTABLE_DIRECTORY}/APPLICATION_METADATA.source"
+
+	# Software installation directory prefix, should be overridable by configure/install script
+	# Scope of external project
+	#shellcheck disable=SC1090,SC1091
+	source "${RUNTIME_EXECUTABLE_DIRECTORY}/SOFTWARE_INSTALLATION_PREFIX_DIR.source" || true
+	SHC_PREFIX_DIR="$(realpath --strip "${RUNTIME_EXECUTABLE_DIRECTORY}/${SOFTWARE_INSTALLATION_PREFIX_DIR:-.}")" # By default we expect that the software installation directory prefix is same directory as script
+	readonly SHC_PREFIX_DIR
+
+	# Scope of external project
+	#shellcheck disable=SC1090,SC1091
+	source "${SHC_PREFIX_DIR}/SOFTWARE_DIRECTORY_CONFIGURATION.source"
+
+	if [ ! -d "${SDC_RELEASE_DIR}" ]; then
+		mkdir --parents "${SDC_RELEASE_DIR}"
+	fi
+	tar\
+		--create\
+		--verbose\
+		--bzip2\
+		--directory "${SHC_PREFIX_DIR}"\
+		--file "${SDC_RELEASE_DIR}/${META_APPLICATION_NAME}-${package_version}.tar.bz2"\
+		--\
+		*.source\
+		"install.bash"\
+		"README.markdown"\
+		"Source Code"\
+		"Template Setup for KDE"\
+		"Pictures"
+
+	exit 0
+}; declare -fr init
+
+## This function requires GIT_DIR and GIT_WORK_TREE to be set and exported
 determine_package_revision(){
-	if [ ! -d "${RUNTIME_SCRIPT_DIRECTORY}/.git" ]; then
+	if [ ! -d "${GIT_DIR}" ]; then
 		# FIXME: This is a source tarball without git repository, currently we don't know how to deal with this case(may requires smudge filter)
-		printf "unknown-%s" "$(basename "${RUNTIME_SCRIPT_DIRECTORY}")"
+		printf 'unknown-%s' "$(basename "${GIT_WORK_TREE}")"
 		return 0
 	fi
 
@@ -48,45 +133,94 @@ determine_package_revision(){
 
 	if ! git rev-parse --verify HEAD &>/dev/null; then
 		# git repository is newly initialized
-		printf "not-version-controlled"
+		printf not-version-controlled
 		return 0
 	else
 		# Best effort to describe the revision
 		# version control - How do you achieve a numeric versioning scheme with Git? - Software Engineering Stack Exchange
  		# https://softwareengineering.stackexchange.com/questions/141973/how-do-you-achieve-a-numeric-versioning-scheme-with-git
-		printf "%s" "$(git describe --tags --dirty --always)"
+		printf '%s' "$(git describe --tags --dirty --always)"
 		return 0
 	fi
-}
-readonly -f determine_package_revision
+}; readonly -f determine_package_revision
 
-## init function: program entrypoint
-init(){
-	for commandline_argument in "${@}"; do
-		if [ "${commandline_argument}" == "--help" ] || [ "${commandline_argument}" == "-h" ]; then
-			printf "This program doesn't have any command line arguments.\n"
-			printf "\n"
-			exit 0
+## Traps: Functions that are triggered when certain condition occurred
+## Shell Builtin Commands » Bourne Shell Builtins » trap
+trap_errexit(){
+	printf 'An error occurred and the script is prematurely aborted\n' 1>&2
+	return 0
+}; declare -fr trap_errexit; trap trap_errexit ERR
+
+trap_exit(){
+	return 0
+}; declare -fr trap_exit; trap trap_exit EXIT
+
+trap_return(){
+	local returning_function="${1}"
+
+	printf 'DEBUG: %s: returning from %s\n' "${FUNCNAME[0]}" "${returning_function}" 1>&2
+}; declare -fr trap_return
+
+trap_interrupt(){
+	printf '\n' # Separate previous output
+	printf 'Recieved SIGINT, script is interrupted.' 1>&2
+	return 1
+}; declare -fr trap_interrupt; trap trap_interrupt INT
+
+print_help(){
+	printf 'Currently no help messages are available for this program\n' 1>&2
+	return 0
+}; declare -fr print_help;
+
+process_commandline_parameters() {
+	if [ "${#RUNTIME_COMMANDLINE_PARAMETERS[@]}" -eq 0 ]; then
+		return 0
+	fi
+
+	# modifyable parameters for parsing by consuming
+	local -a parameters=("${RUNTIME_COMMANDLINE_PARAMETERS[@]}")
+
+	# Normally we won't want debug traces to appear during parameter parsing, so we  add this flag and defer it activation till returning(Y: Do debug)
+	local enable_debug=N
+
+	while true; do
+		if [ "${#parameters[@]}" -eq 0 ]; then
+			break
+		else
+			case "${parameters[0]}" in
+				--help\
+				|-h)
+					print_help;
+					exit 0
+					;;
+				--debug\
+				|-d)
+					enable_debug=Y
+					;;
+				*)
+					printf 'ERROR: Unknown command-line argument "%s"\n' "${parameters[0]}" >&2
+					return 1
+					;;
+			esac
+			# shift array by 1 = unset 1st then repack
+			unset 'parameters[0]'
+			if [ "${#parameters[@]}" -ne 0 ]; then
+				parameters=("${parameters[@]}")
+			fi
 		fi
 	done
 
-	# Software installation directory prefix, should be overridable by configure/install script
-	# Scope of external project
-	#shellcheck disable=SC1090,SC1091
-	source "${RUNTIME_SCRIPT_DIRECTORY}/SOFTWARE_INSTALLATION_PREFIX_DIR.source" || true
-	SHC_PREFIX_DIR="$(realpath --strip "${RUNTIME_SCRIPT_DIRECTORY}/${SOFTWARE_INSTALLATION_PREFIX_DIR:-.}")" # By default we expect that the software installation directory prefix is same directory as script
-	readonly SHC_PREFIX_DIR
-
-	# Scope of external project
-	#shellcheck disable=SC1090,SC1091
-	source "${SHC_PREFIX_DIR}/SOFTWARE_DIRECTORY_CONFIGURATION.source"
-
-	if [ ! -d "${SDC_RELEASE_DIR}" ]; then
-		mkdir --parents "${SDC_RELEASE_DIR}"
+	if [ "${enable_debug}" = Y ]; then
+		trap 'trap_return "${FUNCNAME[0]}"' RETURN
+		set -o xtrace
 	fi
-	tar --create --verbose --bzip2 --directory "${SHC_PREFIX_DIR}" --file "${SDC_RELEASE_DIR}/${APPLICATION_IDENTIFIER}-$(determine_package_revision).tar.bz2" -- *.source "install.bash" "README.markdown" "Source Code" "Template Setup for KDE" "Pictures"
+	return 0
+}; declare -fr process_commandline_parameters;
 
-	exit 0
-}
-readonly -f init
 init "${@}"
+
+## This script is based on the GNU Bash Shell Script Template project
+## https://github.com/Lin-Buo-Ren/GNU-Bash-Shell-Script-Template
+## and is based on the following version:
+## GNU_BASH_SHELL_SCRIPT_TEMPLATE_VERSION="@@GBSST_VERSION@@"
+## You may rebase your script to incorporate new features and fixes from the template
