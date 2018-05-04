e70ba86828361f0b1d3306b5a8c97cfc41a85d2b
林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>
2018-05-04 20:13:53 +0800

    primitive: strict_mode: Eliminate multiple calls to `set`
    Signed-off-by: 林博仁(Buo-ren Lin) <Buo.Ren.Lin@gmail.com>

--- Source Code/Shell Script(GNU Bash)(primitive).template.bash
+++ Source Code/Shell Script(GNU Bash)(primitive).template.bash
@@ -4,10 +4,11 @@
 
 ## Makes debuggers' life easier - Unofficial Bash Strict Mode
 ## BASHDOC: Shell Builtin Commands - Modifying Shell Behavior - The Set Builtin
-set -o errexit
-set -o errtrace
-set -o nounset
-set -o pipefail
+set \
+	-o errexit \
+	-o errtrace \
+	-o nounset \
+	-o pipefail
 
 ## Runtime Dependencies Checking
 declare\
